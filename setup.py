abda671a1c51af67108ead0178bc68fdd06cbc3e
MarcoGorelli <>
2022-12-07 10:37:13 +0000

    Mirror: 18.4a2

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ from setuptools import setup
 setup(
     name='pre_commit_placeholder_package',
     version='0.0.0',
-    install_requires=['black==18.4a1'],
+    install_requires=['black==18.4a2'],
 )
